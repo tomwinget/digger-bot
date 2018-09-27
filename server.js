be75526b35c632708c9837014fd657090e3db4d3
Tom Winget <towinget@cisco.com>
2018-09-26 17:17:56 -0700

    changing shrug to response instead of message edit :(

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ client.on('message', message => {
     message.channel.send(emojiList);
   }
   if (message.content.toLowerCase() === "peanutbutter" || message.content.toLowerCase() === "shrug"){
-          message.edit("¯\_(ツ)_/¯");
+          message.channel.send("¯\_(ツ)_/¯");
   }
   for (var i = 0; i< emojilist.length; i++){
     if(emojilist[i].name.toLowerCase() === 'fortnite' && message.content.toLowerCase().includes('retard')){
